803a31e231b739dadb9f7da55804249402d8de6b
rens-ivo <116071167+rens-ivo@users.noreply.github.com>
2022-10-18 08:16:04 -0400

    marss

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,10 +2,13 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello Woooooorld!\n');
 });
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
 
+app.get('/mars', function (req, res) {
+  res.send('Hello Mars!!!\n');
+});
