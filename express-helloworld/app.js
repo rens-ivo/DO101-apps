326688ce29a9fca424bf4307865de65f9453ac29
rens-ivo <116071167+rens-ivo@users.noreply.github.com>
2022-10-18 08:46:43 -0400

    perzik

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Woooooorld!\n');
+  res.send('Je bent een perzik!\n');
 });
 
 app.listen(8080, function () {
